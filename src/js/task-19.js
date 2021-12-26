5d133a30bfa5902979ab5df8181e2b74dea116ac
Kristina <ktuzova1901@gmail.com>
2021-12-27 00:52:56 +0200

    19th

--- src/js/task-19.js
+++ src/js/task-19.js
@@ -4,3 +4,10 @@
 
 // Пример:
 //  adjacentElementsProduct([1, 2, 3]); ==> return 6
+function  adjacentElementsProduct(arr) {
+    let newarr = [...arr]
+    let res = newarr.sort((a, b) => b - a).slice(0, 2);
+    let sum = res[0] * res[1]
+  return sum;
+}
+console.log(adjacentElementsProduct([1,2,3]))
