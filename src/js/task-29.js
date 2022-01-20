c0b3d853b0acb28a268704795b6fa84ac0569558
Kristina <ktuzova1901@gmail.com>
2022-01-20 18:33:07 +0200

    29th task

--- src/js/task-29.js
+++ src/js/task-29.js
@@ -1,63 +1,134 @@
-function renderField(field) {
-    let output = '';
-    let newarr = []
-    let str = ['|','|','\n','---+---+---','\n','|','|','\n','---+---+---','\n','\n','|','|']
-    let number = [...field]
-    
-    
+function gameStatus(field) {
+    let zeroarr = []
+    let nozeroarr = []
+    let newarr = [] 
+    let somarr = []
+    let orrarr = []
+    let krr = []
+    let anotherarr = []
+    let fg = []
+    let k = []
+    let l = []
+    let a = []
+   
     for (let i = 0; i < field.length; i++) {
           
+        if (field[i] === 0) {
+            zeroarr.push(field[i])
+        }
+       
+
         if (field[i] !== 0) {
-            newarr.push(field[i])
+            nozeroarr.push(field[i])
         }
-              
-    }
-    for (let i = 0; i < number.length; i++) {
-          
-        if (number[i] === 1) {
-            number[i] = 'x'
-        }
-        if (number[i] === -1) {
-            number[i] = '0'
-        }
-        if (number[i] === 0) {
-            number[i] = ' '
-        }
-              
-    }
-   
-    //  let k = [number[0], str[0], number[1], str[1], number[2], str[2], number[3],
-    //     str[3], number[4], str[4], number[5], str[5], number[6], str[6], number[7], str[7], number[8], str[8], str[9], str[10],].join(' ')
-    
-    
 
 
-
-
-    if (newarr.length < 1) {
-         output =`   ${'|   |'}\n${'---+---+---'}\n   ${'|   |'}\n${'---+---+---'}\n   ${ '|   |'}`
+        if ((field[0] === field[4] && field[8] === field[0])) {
+            newarr.push(field[0])
+        }
+        
+        if (field[3] === field[4] && field[4] === field[3]) {
+             somarr.push(field[3])
+        }
+             if (field[0] === field[1] && field[2] === field[0]) {
+             krr.push(field[0])
+        }
+        
+        if ((field[2] === field[4] && field[7] === field[2])) {
+           orrarr.push(field[0])
+        }
+            if ((field[6] === field[7] && field[6] === field[8])) {
+         anotherarr.push(field[0])
+        }
+             if ((field[0] === field[3] && field[6] === field[0])) {
+         l.push(field[0])
+        }
+           if ((field[2] === field[5] && field[8] === field[2])) {
+         a.push(field[0])
+        }
+           
+         
        
+            
+        }
+    
+    
+    
+    
+    
+    
+
+    if (nozeroarr.length == 9) {
+           return 'end'
+    }
+    else if (zeroarr.length == 9) {
+           return 'turn'
     }
     
-    
-  
+    else if (newarr[0] == -1) {
+        return '0'
+    }
+    else if (newarr[0] == 1) {
+        return 'x'
+    }
+    else if (somarr[0] == -1) {
+        return '0'
+    }
+    else if (somarr[0] == 1) {
+        return 'x'
+    }
+    else if (orrarr[0] == -1) {
+        return '0'
+    }
+    else if (orrarr[0] == 1) {
+        return 'x'
+    }
+    else if (krr[0] == -1) {
+        return '0'
+    }
+    else if (krr[0] == 1) {
+        return 'x'
+    }
+    else if (anotherarr[0] == -1) {
+        return '0'
+    }
+    else if (anotherarr[0] == 1) {
+        return 'x'
+    }
+    else if (fg[0] == -1) {
+        return '0'
+    }
+    else if (fg[0] == 1) {
+        return 'x'
+    }
+    else if (k[0] == -1) {
+        return '0'
+    }
+    else if (k[0] == 1) {
+        return 'x'
+    }
+    else if (l[0] == -1) {
+        return '0'
+    }
+    else if (l[0] == 1) {
+        return 'x'
+    }
+    else if (a[0] == 1) {
+        return 'x'
+    }
+    else if (a[0] == -1) {
+        return '0'
+    }
     else {
-
-        output = ` ${number[0]} ${'|'} ${number[1]} ${'|'} ${number[2]} \n${'---+---+---'}\n ${number[3]} ${'|'} ${number[4]} ${'|'} ${number[5]} \n${'---+---+---'}\n ${number[6]} ${'|'} ${number[7]} ${'|'} ${number[8]}`
-    
-       
-    } 
-    
-    
-    
-  
-   
-    
-    
-    return output
+        return 'turn'
+    }
     
 }
-console.log(renderField([0, 0, 0, 0, 0, 0, 0, 0, 0]))
-console.log(renderField([0, -1, 0, 0, 1, 0, 0, 0, 0]))
-console.log(renderField([1, -1, 0, 0, 1, 0, 0, 0, -1]))
-
+console.log(gameStatus([ 0, 0, 0, 0, 0, 0, 0, 0, 0,]))
+console.log(gameStatus([ 1, -1, 1,-1, 1, -1,-1, 1, -1,]))
+console.log(gameStatus([1, 0, -1, 0, 1, -1, 0, 0, 1,]))
+console.log(gameStatus([1, 1, -1, -1, -1, -1, 1, 1, 0,]))
+console.log(gameStatus([1, -1, 0, 1, -1, -1, 1, 1, 0,]))
+console.log(gameStatus([1, -1, -1, -1, 1, 0, 1, 0, 1,]))
+console.log(gameStatus([0, 0, -1, 0, 1, 0, 0, 0, 0]))
+console.log(gameStatus([ 1, -1, 0, 1, -1, -1, 1, 1, 0,]))
